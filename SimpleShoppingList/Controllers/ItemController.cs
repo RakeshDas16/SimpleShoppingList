481fbc94f6dba3a4075290ca55ba502f8be508b4
Rakesh Das <imrakesh.gir@gmail.com>
2020-08-08 15:32:25 +0530

    co

---
+++ SimpleShoppingList/Controllers/ItemController.cs
@@ -0,0 +1,67 @@
+﻿using SimpleShoppingList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleShoppingList.Controllers
+{
+    public class ItemController : ApiController
+    {
+        // GET: api/Item
+
+        // POST: api/Item
+        public IHttpActionResult Post([FromBody]Item item)
+        {
+            ShoppingList shopp = ShoppingListController.shoppingLists.Where(c => c.Id == item.ShoppingListId).FirstOrDefault();
+
+            if(shopp ==null)
+            {
+                return NotFound();
+            }
+            item.Id = shopp.Items.Max(i => i.Id) + 1;
+            shopp.Items.Add(item);
+
+            return Ok(shopp);
+        }
+
+        // PUT: api/Item/5
+        public IHttpActionResult Put(int id, [FromBody]Item item)
+        {
+            ShoppingList shopp = ShoppingListController.shoppingLists.Where(c => c.Id == item.ShoppingListId).FirstOrDefault();
+
+            if (shopp == null)
+            {
+                return NotFound();
+            }
+            Item changedItem = shopp.Items.Where(i => i.Id == id).FirstOrDefault();
+
+            if(changedItem==null)
+            {
+                return NotFound();
+            }
+            changedItem.Checked = item.Checked;
+
+            return Ok(shopp);
+        }
+
+        // DELETE: api/Item/5
+        public IHttpActionResult Delete(int id)
+        {
+            ShoppingList shopp = ShoppingListController.shoppingLists[0];
+
+            Item item = shopp.Items.FirstOrDefault(i => i.Id == id);
+
+            if(item==null)
+            {
+                return NotFound();
+
+            }
+            shopp.Items.Remove(item);
+
+            return Ok(shopp);
+        }
+    }
+}
--- SimpleShoppingList/Scripts/app.js
+++ SimpleShoppingList/Scripts/app.js
@@ -1,11 +1,26 @@
 ﻿var currentList = {};
 
 function createShoppingList() {
+  
     currentList.name = $("#spListName").val();
     currentList.items = new Array();
-    //Web Service Call
 
-    showShoppingList();
+
+    //Web Service Call
+    $.ajax({
+        type: "POST",
+        datatype: "json",
+        url: "api/ShoppingListsEF/",
+        data: currentList,
+        success: function (result) {
+            currentList = result;
+            showShoppingList();
+            history.pushState({ id: result.id }, result.name, "?id=" + result.id);
+         
+        }
+    });
+
+   
 }
 function showShoppingList() {
     $("#shopListTitle").html(currentList.name);
@@ -14,7 +29,9 @@ function showShoppingList() {
     $("#createListDiv").hide();
     $("#shopListDiv").show();
 
+    $('#newItemName').val("");
     $('#newItemName').focus();
+    $('#newItemName').unbind("keyup");
     $("#newItemName").keyup(function (event) {
         if (event.keyCode == 13) {
             addItem();
@@ -26,11 +43,23 @@ function showShoppingList() {
 function addItem() {
     var newItem = {};
     newItem.name = $("#newItemName").val();
-    currentList.items.push(newItem);
-    console.info(currentList);
+    newItem.shoppingListId = currentList.id;
 
-    drawItems();
-    $("#newItemName").val("");
+    $.ajax({
+        type: "POST",
+        datatype: "json",
+        url: "api/ItemsEF",
+        data: newItem,
+        success: function (result) {
+            currentList = result;
+
+            drawItems();
+            $("#newItemName").val("");
+
+        }
+    });
+
+
 }
 
 function drawItems() {
@@ -40,42 +69,94 @@ function drawItems() {
         var currentItem = currentList.items[i];
         var $li = $("<li>").html(currentItem.name).attr("id", "item_" + i);
         console.info($li);
-        var $deletBtn = $("<button onclick='deleteItem(" + i + ")'>D<button>").appendTo($li);
-        var $checkBtn = $("<button onclick='checkItem(" + i +")'>C<button>").appendTo($li);
+        var $deletBtn = $("<button onclick='deleteItem(" + currentItem.id + ")'>D<button>").appendTo($li);
+        var $checkBtn = $("<button onclick='checkItem(" + currentItem.id + ")'>C<button>").appendTo($li);
 
+
+        if (currentItem.checked) {
+            $li.addClass("checked");
+        }
         $li.appendTo($list);
 
     }
-} function deleteItem(index) {
-    currentList.items.splice(index, 1);
-    drawItems();
+
+   
+
+} function deleteItem(itemId) {
+    $.ajax({
+        type: "DELETE",
+        datatype: "json",
+        url: "api/ItemsEF/" + itemId,
+        success: function (result) {
+            currentList = result;
+
+            drawItems();
+
+
+        }
+    });
 }
 
-function checkItem(index) {
-    if ($("#item_" + index).hasClass("checked")) {
+function checkItem(itemId) {
+   /* if ($("#item_" + index).hasClass("checked")) {
         $("#item_" + index).removeClass("checked");
     }
     else {
         $("#item_" + index).addClass("checked");
+    }*/
+
+    var changedItem = {};
+
+    for(var i = 0; i < currentList.items.length; i++)
+    {
+        if (currentList.items[i].id == itemId) {
+            changedItem = currentList.items[i];
+        }
+
     }
+
+    changedItem.checked = !changedItem.checked;
+
+    $.ajax({
+        type: "PUT",
+        datatype: "json",
+        url: "api/ItemsEF/" + itemId,
+        data: changedItem,
+        success: function (result) {
+            changedItem = result;
+
+            drawItems();
+           
+
+        }
+    });
+
 }
 
 function getShopListById(id) {
 
-    console.info(id);
-    currentList.name = "Mock Shopping List";
-    currentList.items = [
-        { name: "Milk" },
-        { name: "Chocolate" },
-        { name: "Strawberries" }
-    ];
+    $.ajax({
+        type: "GET",
+        datatype: "json",
+        url: "api/ShoppingListsEF/" + id,
+        success: function (result) {
+            currentList = result;
+            showShoppingList();
+            drawItems();
+        }
+       
 
-    showShoppingList();
-    drawItems();
+    });
+
 }
-$(document).ready(function() {
-    console.info("ready");
+
+function hideShoppingList() {
+    $("#createListDiv").show();
+    $("#shopListDiv").hide();
+
+    $('#spListName').val("");
     $('#spListName').focus();
+    $('#spListName').unbind("keyup");
     $("#spListName").keyup(function (event) {
         if (event.keyCode == 13) {
             createShoppingList();
@@ -83,11 +164,28 @@ $(document).ready(function() {
 
     });
 
+}
+
+$(document).ready(function() {
+    console.info("ready");
+
+    hideShoppingList();
+   
+
     var pageUrl = window.location.href;
     var idIndex = pageUrl.indexOf("?id=");
 
     if (idIndex != -1) {
         getShopListById(pageUrl.substring(idIndex + 4));
+    }
+
+    window.onpopstate = function (event) {
+        if (event.state == null) {
+            hideShoppingList();
+        }
+        else {
+            getShopListById(event.state.id);
+        }
     }
 });
 
